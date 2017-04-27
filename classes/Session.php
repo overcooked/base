b1560372b41358f60bcc2ca449dcdbc883f14b2e
Justin-Leung <lngventure@gmail.com>
2017-04-26 21:11:54 -0700

    Cleaned up documentation.

--- classes/Session.php
+++ classes/Session.php
@@ -1,44 +1,44 @@
 <?php
 /**
-  * The Session class allows for easy assignment
-  * and deletion of the session variable.
-  * @author Justin leung
-  * @version 1.0
-  */
+ * The Session class applies setters and getters
+ * for easy access to the $_SESSION variable.
+ * @author Team Point.
+ * @version 1.0
+ */
 class Session {
 
   /**
-    * Sets a session variable with a name and value.
-    * @param name - The name of the session variable.
-    * @param value - The value of the session variable.
-    * @return the value of the session you created.
-    */
+   * Sets a session variable with a name and value.
+   * @param String $name  - The name of the session variable.
+   * @param String $value - The value of the session variable.
+   */
   public static function set($name, $value) {
     return $_SESSION[$name] = $value;
   }
 
   /**
-    * Returns a session variables value.
-    * @param name - The name of the session variable.
-    * @return the value of a session variable.
-    */
+   * Returns a session variables value.
+   * @param  String $name - The name of the session variable.
+   * @return String       - The value of the session variable.
+   */
   public static function get($name) {
     return $_SESSION[$name];
   }
 
   /**
-    * Checks to see if session variable is set.
-    * @param name - The name of the session variable.
-    * @return true if exists, false if not.
-    */
+   * Checks to see if session variable is set.
+   * @param  String $name - The name of the session variable.
+   * @return boolean      - True if the session exists, false if not.
+   */
   public static function exists($name) {
     return (isset($_SESSION[$name])) ? true : false;
   }
 
   /**
-    * Deletes a session variable if it exists.
-    * @param name - The name of the session variable.
-    */
+   * Deletes a session variable if it exists
+   * @param  String $name - The name of the session variable.
+   * @return void
+   */
   public static function delete($name) {
     if(self::exists($name)) {
       unset($_SESSION[$name]);
@@ -46,12 +46,12 @@ class Session {
   }
 
   /**
-    * Allows the ability to flash messages onto
-    * pages, that only show once flashed.
-    * @param name - The name of the session/flash.
-    * @param contents - The contents of the flash message.
-    * @return the contents of the message to be flashed.
-    */
+   * Flash is used to send a temporary message
+   * to another page that is flashed once.
+   * @param  String $name     - The name of the session variable.
+   * @param  String $contents - The flash message to be saved.
+   * @return String           - The contents of the flash message.
+   */
   public static function flash($name, $contents = '') {
     if(self::exists($name)) {
       $session = self::get($name);
@@ -65,5 +65,4 @@ class Session {
   }
 
 }
-
 ?>

9bae433ff1f29b5e3b2d5da855eb38e05fa4ab77
Justin-Leung <lngventure@gmail.com>
2017-04-27 12:05:54 -0700

    Documentation added.

--- classes/Hash.php
+++ classes/Hash.php
@@ -1,14 +1,34 @@
 <?php
+/**
+ * The Hash class is used to make hash values,
+ * and generate salts for securing passwords.
+ * @author Team Point.
+ * @version 1.0
+ */
 class Hash {
 
+  /**
+   * Creates a hash value encrypted with sha256.
+   * @param  string $string - The value to be hashed.
+   * @param  string $salt   - The salt to be included in the hash.
+   * @return string         - The hash value that was made.
+   */
   public static function make($string, $salt = '') {
      return hash('sha256', $string . $salt);
   }
 
-  public static function salt($length) {
+  /**
+   * Generates a salt value with a length of 32.
+   * @return [type]         [description]
+   */
+  public static function salt() {
     return utf8_encode(openssl_random_pseudo_bytes(32));
   }
 
+  /**
+   * Generates a unique random hash with no salt.
+   * @return [type] [description]
+   */
   public static function unique() {
     return self::make(uniqid());
   }

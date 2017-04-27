4480bb8a094b9e99ae622bd9521ebcbecaef598a
Justin-Leung <lngventure@gmail.com>
2017-04-27 13:39:38 -0700

    Documentation added.

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Used to escape strings and sql queries.
+ * @param  string $string - The string to be escaped.
+ * @return string         - The escaped string.
+ */
 function escape($string) {
   return htmlentities($string, ENT_QUOTES, 'UTF-8');
 }

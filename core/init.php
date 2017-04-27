10b0f9f462b61206d0cfb3411ff23b3e5a86aa00
Justin-Leung <lngventure@gmail.com>
2017-04-26 21:48:06 -0700

    Documentation added.

--- core/init.php
+++ core/init.php
@@ -1,11 +1,28 @@
 <?php
+/**
+ * The init.php was created to initialize every other file
+ * with useful imports and data to support code re-use.
+ * @author Team Point.
+ *
+ *   - Supported Intialization Features -
+ *   1.) PHP Session starting.
+ *   2.) PHP Error Displaying Enabled.
+ *   3.) GLOBAL Config Information. (DB info, Global Variable Names)
+ *   4.) Sanitization Functions Import.
+ *   5.) Autoloader To Import Classes.
+ *   6.) Login Cookie Checker With Auto Login.
+ *
+ */
+
+/** 1.) Start the session for the users browser. */
 session_start();
 
+/** 2.) Turn on error display options. */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Global variable of all related data for a user session.
+/** 3.) Global information about shared data */
 $GLOBALS['config'] = array(
   'mysql' => array(
     'host' => 'localhost',
@@ -23,10 +40,27 @@ $GLOBALS['config'] = array(
   )
 );
 
-// Allows use to autoload classes only when an instance of that class is used.
+/** 4.) Include sanitize functions to secure inputs. */
+require_once 'functions/sanitize.php';
+
+/** 5.) Automatic class loader. */
 spl_autoload_register(function($class) {
   require_once('classes/' . $class . '.php');
 });
 
-require_once 'functions/sanitize.php';
+/** 6.) Checks for saved cookie, and logs user in automatically. */
+if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
+
+  // Get the users saved cookie value.
+  $hash = Cookie::get(Config::get('remember/cookie_name'));
+
+  // Check if that cookie is expired or exists.
+  $hash_check = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+
+  // If cookie is valid, log the user in automatically.
+  if($hash_check->count()) {
+    $user = new User($hash_check->results()[0]->user_id);
+    $user->login();
+  }
+}
 ?>

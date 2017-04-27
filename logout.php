56a4b280f262b9499450d7d88541b79ac8a9dd5a
Justin-Leung <lngventure@gmail.com>
2017-04-27 13:55:45 -0700

    Small changes.

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'core/init.php';
+require_once (getcwd() . "/core/init.php");
 
 $user = new User();
-if($user->isLoggedIn()) {
+if($user->is_logged_in()) {
   $user->logout();
 }
 

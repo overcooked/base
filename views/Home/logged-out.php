2dff347cc93c5feb224dbea08e28bd9b1b445801
dominiclrt <dominiclrt@gmail.com>
2017-05-04 16:13:32 -0700

    index.php text updated

--- views/Home/logged-out.php
+++ views/Home/logged-out.php
@@ -67,28 +67,31 @@ $user = new User();
         <div class="row">
 
           <!-- Link Section #1 -->
+           <div class="col-md-4">
+             <h2 class="font-semibold">Find a Meal</h2>
+             <h5 style="font-family:proximanova-regularitalic">Find food near you</h5>
+             <div class="hoz-divider"></div>
+             <p>Overcooked allows users to meet each other face-to-face for meal pickups. All meals posted are free of charge. Please be aware that by signing up you have agreed to the Terms of Service.</p>
+             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+           </div>
+
+          <!-- Link Section #1 -->
           <div class="col-md-4">
             <h2 class="font-semibold">Reduce Waste</h2>
+            <h5 style="font-family:proximanova-regularitalic">Make a difference</h5>
             <div class="hoz-divider"></div>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>You can reduce the amount of waste you create by choosing what food you throw away. This can be easy and fun – just follow our simple guidelines to reduce your waste at home, school, or work.</p>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
           </div>
 
           <!-- Link Section #1 -->
           <div class="col-md-4">
             <h2 class="font-semibold">Community</h2>
+            <h5 style="font-family:proximanova-regularitalic">Giveback to those around you</h5>
             <div class="hoz-divider"></div>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>Our organization takes donations in the form of fresh-made, leftover, or perishable foods and uploads them online for those in need.<br /> Please refer to our guidelines and privacy policy before posting.</p>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
          </div>
-
-         <!-- Link Section #1 -->
-          <div class="col-md-4">
-            <h2 class="font-semibold">Help People</h2>
-            <div class="hoz-divider"></div>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          </div>
 
         </div>
 

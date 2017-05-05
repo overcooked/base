460fcd5ee84d355823840aa410018c5daaff4782
mac craig <maccraig98@gmail.com>
2017-05-04 22:59:48 -0700

    Remove unused make

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -7,9 +7,5 @@ rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 
-# Install zopfli
-git clone https://code.google.com/p/zopfli/
-cd zopfli
-make
-chmod +x zopfli
+
 cd ..

418d6e7694017c6bc8b6cf38355fcbf393ea89a7
mac craig <maccraig98@gmail.com>
2017-05-04 22:58:01 -0700

    New travis config

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'dev' ] ; then
+    cd html
+    git init
 
-# print outputs and exit on first failure
-# mac@138.68.237.56:/var/www/overcooked.ca
-set -xe
-
-if [ $TRAVIS_BRANCH == "dev" ] ; then
-
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@138.68.237.56:/var/www/overcooked.ca"
+    git remote add deploy "deploy@138.68.237.56:/var/www/html"
     git config user.name "Travis CI"
     git config user.email "travis@overcooked.ca"
 
-    # commit compressed files and push it to remote
     git add .
-    git status # debug
-    git commit -m "Deploy built files"
-    git push -f deploy HEAD:master
-
-    echo "DONE!"
-
+    git commit -m "Deploy"
+    git push --force deploy dev
 else
-
-    echo "No deploy script for branch '$TRAVIS_BRANCH'"
-
+    echo "Not deploying, since this branch isn't dev."
 fi

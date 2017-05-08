baaee5339614ed1ba6b6a9f5f68c06b45261d62f
Piyotr Kao <piyotrkao1998@Outlook.com>
2017-05-07 21:09:25 -0700

    Updated how it works page

--- views/Howitworks/howitworks.php
+++ views/Howitworks/howitworks.php
@@ -26,6 +26,19 @@ $user = new User();
             ::-webkit-scrollbar {
                 display: none;
             }
+            hr {
+                width: 60%;
+            }
+            .panel-heading {
+                color: #545454; 
+                font-size: 40px;
+                border:none; padding-top: 14px;
+                font-family: proximanova-bold;
+                text-transform: uppercase;
+            }
+            .panel-body {
+                font-size: 15px;
+            }
         </style>
     </head>
 
@@ -50,6 +63,8 @@ $user = new User();
                     </div>
                 </div>
             </div>
+            
+            <!-- Main content -->
             <div class="container" style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15); background-color: #fff; border-radius: 3px;">
                 
                 <!-- First row -->
@@ -58,7 +73,7 @@ $user = new User();
                     <!-- Overcooked logo -->
                     <div class="col-md-3 col-md-offset-2">
                         <br><br><br><br><br><br>
-                        <img style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3); background-color: #333; color: #fff; text-align: center; border-radius: 500px; padding-top: 10px; padding-left: 23px; padding-right: 7px; padding-bottom: 3px;" src="/public/assets/images/OvercookedVector.svg" width="263px">
+                        <img style="box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3); background-color: #333; color: #fff; border-radius: 500px; padding-top: 10px; padding-left: 23px; padding-right: 7px; padding-bottom: 3px;" src="/public/assets/images/OvercookedVector.svg" width="263px">
                         <br>
                     </div>
                     
@@ -66,10 +81,44 @@ $user = new User();
                     <div class="col-md-5" style="margin-left: 40px;">
                         <br><br><br><br><br>
                             <div class="row">
-                            <div class="panel-heading" style="color: #545454; font-size: 40px; border:none; padding-top: 14px; font-family: proximanova-bold; text-transform: uppercase;">Overview</div>
-                            <div class="panel-body" style="font-size: 15px;">
+                            <div class="panel-heading">Overview</div>
+                            <div class="panel-body">
                                 Overcooked is a web app that allows users on the platform to post their leftover foods, whether its from a house party, or from your fridge, you can post a variety of leftover food on the site.
+                                <br><br>
+                                The website looks to reduce as much food waste as possible along with helping out people in need.
+                                <br><br>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <br><br>
+                <hr>
+                <br><br>
+                
+                <!-- Second row -->
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-1">
+                        <h1 style="font-size: 48px; font-family: proximanova-bold; letter-spacing: 3px;">
+                            FEATURES
+                        </h1>
+                    </div>
+                    <div class="col-md-5" style="margin-left: 40px;">
+                        <div class="panel-heading">Tags</div>
+                        <div class="panel-body">
+                            Every post will contain a tag that relates to the post in some way.
+                            <br><br>
+                            Ex. Indian, Chinese, Vegetarian, Vegan
+                        </div>
+                        <div class="panel-heading">Search</div>
+                        <div class="panel-body">
+                            The search feature will help users filter the posts to a contain a certain tag or multiple tags.
+                        </div>
+                        <div class="panel-heading">Location</div>
+                        <div class="panel-body">
+                            Every post will have a location, there will be a range finder for locations. Users can locate postings near them.
+                            <br><br>
+                            Ex. 5Km, 10Km radius
                         </div>
                     </div>
                 </div>

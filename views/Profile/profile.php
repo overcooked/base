831560bc3afb33dafe2b5908b958eab2862c1883
mac craig <maccraig98@gmail.com>
2017-05-11 14:33:22 -0700

    Profile page mobile improvements

--- views/Profile/profile.php
+++ views/Profile/profile.php
@@ -19,6 +19,49 @@ $user = new User();
     <!-- Style Files. -->
     <link rel="stylesheet" href="/public/css/profile/profile.css">
 
+    <!-- Script Files. -->
+    <script src="/public/js/show-more.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Show more / Show less script -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+        var showChar = 100;
+        var ellipsestext = "...";
+        var moretext = "...";
+        var lesstext = "Hide";
+
+
+        $('.more').each(function() {
+            var content = $(this).html();
+
+            if(content.length > showChar) {
+
+                var c = content.substr(0, showChar);
+                var h = content.substr(showChar, content.length - showChar);
+
+                var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+                $(this).html(html);
+            }
+
+        });
+
+        $(".morelink").click(function(){
+            if($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                $(this).html(moretext);
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+            }
+            $(this).parent().prev().toggle();
+            $(this).prev().toggle();
+            return false;
+        });
+      });
+    </script>
+
 
   </head>
   <body>
@@ -28,51 +71,67 @@ $user = new User();
 
     <!-- Main Content // Start -->
     <section class="main">
-      <div class="container profile-area">
-        <div class="col-md-4 left-tab">
-          <img class="profile-pic" src="https://placeholdit.imgix.net/~text?txtsize=19&txt=200%C3%97200&w=200&h=200">
+      <div class="profile-content container profile-area">
+        <div class="left-tab">
+          <img class="profile-pic" src="https://github.com/tehp.png" width="200" height="200">
           <h3>
-          <?php echo $user->data()->user_first; ?>
-          <?php echo $user->data()->user_last; ?>
+          <?php //echo $user->data()->user_first;?>
+          <?php //echo $user->data()->user_last;?>
+          Mackenzie Craig
         </h3>
-        <h4>user_location</h4>
-        <p>This user has not written their bio yet.</p>
-        <hr>
+        <h4>Port Coquitlam</h4>
+        <span class="bio-content more">
+          Enjoys long walks on the beach. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </span>
+
         </div>
-        <div class="col-md-8 right-tab">
+      </div>
+        <div class="split"><p class="font-semibold split-text">Recent Posts</p></div>
+
+        <div class="posts-area">
+
+        <div class="container">
           <?php
           $postings = DB::getInstance()->get('posts', array('user_id', '=', $user->data()->user_id));
 
           if ($postings->count()) {
               foreach ($postings->results() as $post) {
-                  // Get the image for the post.
-              $post_image = DB::getInstance()->get('post_image', array('post_id', '=', $post->post_id));
+
+                // Get the image for the post.
+                $post_image = DB::getInstance()->get('post_image', array('post_id', '=', $post->post_id));
                   $image = '';
+
+                // Save into a variable.
+                if ($post_image->count()) {
+                    $image = $post_image->first();
+                }
+
+                // Format the description.
+                $description = substr($post->post_description, 0, 100) . '..';
+
+                // Convert the date.
+                $post_date = strtotime($post->post_date);
+                  $post_date = date('Y-m-d', $post_date);
+
+                // Get the ID for the posting.
+                $post_listing_url = '/listing.php?post=' . substr($post->post_id, 5);
 
               // Save into a variable.
               if ($post_image->count()) {
                   $image = $post_image->first();
               }
 
-                  echo '
-              <div class="row post">
-              <div class="col-xs-3 hidden-xs">
-              <img class="post-pic" src="' . $image->post_image_url . '" height="150" width="150">
-              </div>
-              <div class="col-xs-9">
-              ';
-                  echo '<h3>';
+                  echo '<div class="item">';
+                  echo '<p class="title">';
                   echo $post->post_title . '<br>';
-                  echo '</h3>';
-                  echo '<h4>';
-                  echo $post->post_description . '<br>';
-                  echo '</h4><hr>';
-                  echo $post->post_pickup_location . '<br>';
-                  echo '<br>';
-                  echo '
-              </div>
-              </div>
-              ';
+                  echo '</p>';
+                  echo '<p class="description">';
+                  echo $description . '<br>';
+                  echo '</p>';
+                  echo $post->post_pickup_location;
+                  echo '<br><small class="stats-text">Type: ' . $post->post_tag . ' | Posted: ' . $post->post_date . '</small';
+                  echo '</div><hr>';
+                  echo '<a href=' . $post_listing_url . ' class="view-btn btn btn-primary">View Listing</a>';
               }
           } else {
               echo 'No posts for this user.';
@@ -86,7 +145,7 @@ $user = new User();
 
 
     <!-- Footer Section -->
-    <?php View::footer(); ?>
+    <!-- <?php View::footer(); ?> -->
 
   </body>
 </html>

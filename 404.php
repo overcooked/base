22ddeb1a2a32d9e55b519e550ed8987cdfbfd60f
mac craig <maccraig98@gmail.com>
2017-05-11 15:22:40 -0700

    Unfinished 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 404 error page
+ * @uses views/Errors/404 - For the pages UI.
+ */
+
+/** REQUIRED Import For App Initialization. */
+require_once (getcwd() . "/core/init.php");
+
+/** Load the pages view. */
+View::load('404');
+
+?>
---
+++ views/404/404.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 404 Error page view.
+ * @author Team Point.
+ */
+$user = new User();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- General. -->
+    <title>Overcooked: Feed those in need with your extra food.</title>
+    <meta name="description" content="Overcooked lets you give your extra food to people who really need it.">
+
+    <!-- Boiler Plate Tags. -->
+    <?php View::head(); ?>
+
+    <!-- Style Files. -->
+    <link rel="stylesheet" href="/public/css/errors/404.css">
+
+  </head>
+  <body>
+
+    <!-- Header Section -->
+    <?php View::header_logged_in(); ?>
+
+    <!-- Main Content // Start -->
+    <section class="main">
+
+    </section>
+
+
+
+    <!-- Footer Section -->
+    <!-- <?php View::footer(); ?> -->
+
+  </body>
+</html>

0781c54040e32d4d11ebbc9153610af53b230cd8
Justin-Leung <lngventure@gmail.com>
2017-05-24 11:34:57 -0700

    Homepage added benefits section.

--- views/Home/logged-in.php
+++ views/Home/logged-in.php
@@ -111,7 +111,7 @@ $user = new User();
                     // Get the ID for the posting.
                     $post_listing_url = '/listing.php?post=' . substr($post->post_id, 5);
 
-                        echo "
+                    echo "
                       <div class='thumbnail grid-item'>
                         <a href='{$post_listing_url}'>
                           <img src='{$image->post_image_url}' class='img-responsive' alt='Post Image'>

7e6715fedfe80b788e6d508a7e6efecb984a68bf
mac craig <maccraig98@gmail.com>
2017-05-24 20:42:51 -0700

    Make affiliate page responsive

--- views/Affiliated/affiliated.php
+++ views/Affiliated/affiliated.php
@@ -54,19 +54,19 @@ $user = new User();
     </section>
 
     <!-- Lander Section One -->
-    <section id="lander-section-two" style="padding-top: 50px; padding-bottom: 80px;">
+    <section id="lander-section-two" style="padding-top: 50px; padding-bottom: 40px;">
       <div class="container">
 
-        <div class="row" style="padding-left: 30px; padding-right: 30px;">
+        <div class="row affiliate" style="padding-left: 30px; padding-right: 30px;">
 
-          <div class="col-md-3 col-md-offset-1">
+          <div class="hidden-xs hidden-sm col-md-3 col-md-offset-1">
             <img src="/public/assets/images/logo-1.png" style="width: 160px; height: auto; display: block; margin: auto; margin-top: 95px;" />
           </div>
 
           <!-- Info Section #2 -->
            <div class="col-md-7" style="padding-top: 35px;">
              <h2 class="font-semibold" style="letter-spacing: 0.2px; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; color: #626262; text-rendering: optimizeLegibility;">Meal.io</h2>
-             <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">app-devs</h5>
+             <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">Group 29</h5>
              <div class="hoz-divider"></div>
              <p style="letter-spacing: 0.4px; color: #898d94;">
                Meal.io is an application which helps families manage the food in their lives.
@@ -82,20 +82,19 @@ $user = new User();
 
         <div class="container" style="background-color: #343c40;" >
           <!-- Main Information. -->
-          <div class="row" style="background-color: #343c40; padding-left: 30px; padding-right: 30px;">
+          <div class="row affiliate" style="background-color: #343c40; padding-left: 30px; padding-right: 30px;">
 
-            <div class="col-md-3 col-md-offset-1">
-              <img src="/public/assets/images/logo-2.png" style="width: 170px; height: auto; padding-bottom: 40px; display: block; margin: auto; margin-bottom: 65px; margin-top: 61px;" />
+            <div class="hidden-xs hidden-sm col-md-3 col-md-offset-1">
+              <img src="/public/assets/images/logo-2.png" style="width: 170px; height: auto; padding-bottom: 20px; display: block; margin: auto; margin-bottom: 65px; margin-top: 61px;" />
             </div>
 
             <!-- Info Section #1 -->
              <div class="col-md-7" style="padding-top: 75px;">
                <h2 class="font-semibold" style="color: #fff !important; letter-spacing: 0.2px; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; color: #626262; text-rendering: optimizeLegibility;">Food Fall</h2>
-               <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">app-devs</h5>
+               <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">Group 9</h5>
                <div class="hoz-divider"></div>
                <p style="letter-spacing: 0.4px; color: #efefef; font-family: proximanova-light;">
-                 Food Fall is a game about saving food from the despair of unnecessary waste and disposal.<br>
-                 We’ve collaborated with Food Fall because of their hands-on approach to informing people about food waste and how to prevent it. Their colorful and fun gameplay is complimented with tooltips that educate the user about food waste.
+                 Food Fall is a game about saving food from the despair of unnecessary waste and disposal. We’ve collaborated with Food Fall because of their hands-on approach to informing people about food waste and how to prevent it. Their colorful and fun gameplay is complimented with tooltips that educate the user about food waste.
                </p>
              </div>
 
@@ -110,20 +109,19 @@ $user = new User();
 
         <div class="container" >
           <!-- Main Information. -->
-          <div class="row" style="padding-left: 30px; padding-right: 30px;">
+          <div class="row affiliate" style="padding-left: 30px; padding-right: 30px;">
 
-            <div class="col-md-3 col-md-offset-1">
+            <div class="hidden-xs hidden-sm col-md-3 col-md-offset-1">
               <img src="/public/assets/images/logo-3.png" style="display: block; margin: auto; width: 140px; height: auto; margin-top: 95px;" />
             </div>
 
             <!-- Info Section #3 -->
-             <div class="col-md-7" style="padding-top: 35px;">
+             <div class="col-md-7" style="padding-top: 35px; padding-bottom: 100px;">
                <h2 class="font-semibold" style="letter-spacing: 0.2px; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; color: #626262; text-rendering: optimizeLegibility;">RefrigeDate</h2>
-               <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">app-devs</h5>
+               <h5 style="font-family: proximanova-regularitalic; color: #b8bfc2; padding-top: 6px;">Group 20</h5>
                <div class="hoz-divider"></div>
                <p style="letter-spacing: 0.4px; color: #898d94;">
-                 RefrigeDate lets you easily manage food in your household, restaurant, or business.<br>
-                 We’ve chosen RefridgeDate as an affiliate because of their straightforward approach to preventing food waste by guiding users to buy less. The application utilizes an easy to use interface which allows users to easily plan for their next grocery trip.
+                 RefrigeDate lets you easily manage food in your household, restaurant, or business. We’ve chosen RefridgeDate as an affiliate because of their straightforward approach to preventing food waste by guiding users to buy less. The application utilizes an easy to use interface which allows users to easily plan for their next grocery trip.
                </p>
              </div>
 
@@ -134,8 +132,10 @@ $user = new User();
     </section>
 
         <!-- City Skyline -->
-        <div class="row row-no-margin" id="section-one-skyline"></div>
       </div>
+
+      <!-- Footer Section -->
+      <?php View::footer(); ?>
 
     </section>
 
@@ -146,8 +146,7 @@ $user = new User();
     }
     </style>
 
-    <!-- Footer Section -->
-    <?php View::footer(); ?>
+
 
   </body>
 </html>

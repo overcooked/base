a795b4bcadafd75ab14544e0ea61b65540a24d0a
mac craig <maccraig98@gmail.com>
2017-05-25 11:00:53 -0700

    Add propper tags

--- public/js/filter.js
+++ public/js/filter.js
@@ -12,4 +12,28 @@ $(document).ready(function() {
         filter = "fruit";
         $('#search-2').trigger('keyup');
     });
+    $("#rad4").click(function() {
+        filter = "bread";
+        $('#search-2').trigger('keyup');
+    });
+    $("#rad5").click(function() {
+        filter = "poultry";
+        $('#search-2').trigger('keyup');
+    });
+    $("#rad6").click(function() {
+        filter = "frozen";
+        $('#search-2').trigger('keyup');
+    });
+    $("#rad7").click(function() {
+        filter = "fresh";
+        $('#search-2').trigger('keyup');
+    });
+    $("#rad8").click(function() {
+        filter = "packaged";
+        $('#search-2').trigger('keyup');
+    });
+    $("#rad9").click(function() {
+        filter = "expires soon";
+        $('#search-2').trigger('keyup');
+    });
 });
